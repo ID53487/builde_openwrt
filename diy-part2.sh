be5ce0d3579922df0715250189b7bd859ed1e46b
ID53487 <129543194+ID53487@users.noreply.github.com>
2025-06-11 18:23:42 +0800

    Update diy-part2.sh
    ID53487调整内容

--- diy-part2.sh
+++ diy-part2.sh
@@ -10,11 +10,11 @@
 # See /LICENSE for more information.
 #
 
-# Modify default IP
+# Modify default IP /修改 OpenWrt 路由器的默认管理 IP 192.168.1.1为192.168.50.5
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
 
-# Modify default theme
+# Modify default theme/替换主题界面，由默认简洁主题luci-theme-bootstrap替换为科技风的luci-theme-argon
 #sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
 
-# Modify hostname
+# Modify hostname/修改 OpenWrt 系统的默认主机名/由OpenWrt改为P3TERX-Router
 #sed -i 's/OpenWrt/P3TERX-Router/g' package/base-files/files/bin/config_generate
